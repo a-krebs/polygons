02267a6222c3dc483d3d62bcc97ea4fbf4d0a2d0
Aaron Krebs <aaron.krebs@akrebs.ca>
2017-01-27 00:14:50 -0700

    Finalize main.

--- Main.cpp
+++ Main.cpp
@@ -10,10 +10,19 @@ using polygons::Solver;
 int main( int argc, char **argv )
 {
     std::cout << "Starting." << std::endl;
-    std::shared_ptr<Graph> g = Graph::getExampleGraph();
-    Solver s(2);
-    int solutions = s.solve(*g);
-    std::cout << "Finished." << std::endl;
+
+    Graph g = Graph::getExampleGraph();
+
+    // called once for each solution
+    auto callback = [](const Solver& solver, const Graph& g)
+        {
+            std::cout << g << std::endl;
+        };
+
+    // find exactly solutions with exactly two complete triangles
+    Solver s(2, callback);
+
+    int solutions = s.solveRecursiveWithPruning(g);
     std::cout << std::to_string(solutions) << " solutions found." << std::endl;
     return true;
 }
